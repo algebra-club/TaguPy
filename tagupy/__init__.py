0cec02f48457ac18088703e3d3086d8d445ba026
takesei <sei06k14@gmail.com>
2021-08-21 20:17:36 +0900

    :art: Fix Lint

--- tagupy/__init__.py
+++ tagupy/__init__.py
@@ -4,11 +4,11 @@ from . import core
 from . import manager
 from . import simulator
 
-# __all__ = [
-# "core",
-# "manager",
-# "simulator",
-# ]
+__all__ = [
+    "core",
+    "manager",
+    "simulator",
+]
 
 __author__ = """Sei Takeda"""
 __email__ = 'sei06k14@gmail.com'

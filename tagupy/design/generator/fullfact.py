68af875c31623ad73e86c18aab538ef37cfa7d66
takesei <sei06k14@gmail.com>
2021-08-27 00:50:24 +0900

    :wrench: Remove per-file-ignores, set config file @reviewdg

--- tagupy/design/generator/fullfact.py
+++ tagupy/design/generator/fullfact.py
@@ -24,13 +24,11 @@ class FullFact(Generator):
     -----
     Full factorial design creates all the possible combinations of the levels by each factor.
     This design takes n_factors^m_levels experiments if levels are the same numbers.
-    When there are n replicates of complete experiments, there will be n times of the single replication experiments.
+    When number of replications (n_rep) is set as non-zero natural number,
+      there will be n times of the single replication experiments.
     It is recommended to have at least 2 replicates to determine a sum of squares due to error.
     Prasanta Sahoo, Tapan Kr. Barman, Woodhead Publishing Reviews, 2012,Pages 159-226,
     https://doi.org/10.1533/9780857095893.159.
-
-    You can have the replicated FullFactorial Design Matrix
-    at one time by setting n_rep as non-zero natural number.
     '''
 
     def __init__(self, n_rep: int):

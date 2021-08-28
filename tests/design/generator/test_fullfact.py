ef284346348039e43e611a3c6ff6be65a935b267
gk-hazard <06ekoba13@keio.jp>
2021-08-28 17:01:08 +0900

    :recycle: minor update of fullfact test

--- tests/design/generator/test_fullfact.py
+++ tests/design/generator/test_fullfact.py
@@ -7,7 +7,7 @@ from tagupy.design.generator import FullFact
 
 @pytest.fixture
 def correct_input():
-    arg = [[2, 2, 3], [3, 3, 3, 3, 3], [5, 10, 15], [20, 20]]
+    arg = [[2, 2, 3], [3, 3, 3, 3, 3], [5, 10, 15], [20, 20], [2]]
     return arg
 
 
@@ -15,24 +15,36 @@ def test_init_vaild_input():
     arg = [1, 2, 10]
     exp = [1, 2, 10]
     for i, v in enumerate(arg):
-        assert FullFact(v).n_rep == exp[i], \
-            f'Error: self.n_rep expected {exp[i]}, got {FullFact(v).n_rep}'
+        ret = FullFact(v)
+        assert ret.n_rep == exp[i], \
+            f'Error: invalid instance-var n_rep > expected {exp[i]}, got {ret.n_rep}'
 
 
 def test_init_invalid_input():
-    arg = ['a', 3.2, -1, None, [2], np.zeros((2, 3))]
-    with pytest.raises(AssertionError):
-        for i in arg:
+    arg = [
+        ['a', 3.2, None, [2], np.zeros((2, 3)), np.inf, -np.inf],
+        [0, -1]
+    ]
+
+    for i in arg[0]:
+        with pytest.raises(AssertionError) as e:
             FullFact(i)
+        assert f'{type(i)}' in f"{e.value}", \
+            f'ERROR: Assertion msg should contain reasons, got "{e.value}"'
+
+    for i in arg[1]:
+        with pytest.raises(AssertionError) as e:
+            FullFact(i)
+        assert f'{i}' in f"{e.value}", \
+            'ERROR: Assertion msg should contain reasons, got "{e.value}"'
 
 
-def test_init_invalid_output():
+def test_init_valid_output():
     arg = [1, 2, 3, 10]
     for i in arg:
         rep = FullFact(i).n_rep
-        assert isinstance(rep, int), \
-            f'Error: dtype of self.n_rep expected int,\
-                 got {type(rep)}'
+        assert rep == i, \
+            f'Error: invalid dtype > expected {i}, got {rep}'
 
 
 def test_get_exmatrix_valid_output(correct_input):
@@ -42,35 +54,50 @@ def test_get_exmatrix_valid_output(correct_input):
         cor_shape = (np.prod(i) * model.n_rep, len(i))
         ret_shape = model.get_exmatrix(i).shape
         assert cor_shape == ret_shape,\
-            f'Error: shape not matched,\
-        expected: {cor_shape}got: {ret_shape}'
+            f'Error: shape not matched > expected: {cor_shape}, got: {ret_shape}'
+
         cor_len = len(model.get_exmatrix(i))
         ret_len = len(np.unique(model.get_exmatrix(i), axis=0)) * model.n_rep
         assert cor_len == ret_len,\
-            f'Error: Array has duplicated rows,\
-        expected: {cor_len} unique rows, got: {ret_len}'
+            f'Error: Array has duplicated rows > expected: {cor_len} unique rows, got: {ret_len}'
+
         for idx, j in enumerate(i):
             cor_ele = list(range(j))
             ret_ele = np.unique(model.get_exmatrix(i)[:, idx])
             assert np.array_equal(cor_ele, ret_ele),\
-                f'Error: column has unexpected elements,\
-                column{idx}, expected: {cor_ele}, got: {ret_ele}'
+                f'Error: Unexpected elements in Column[{idx}] > expected: {cor_ele}, got: {ret_ele}'
 
 
 def test_getexmatrix_invalid_input():
-    arg = ['a', 3.2, -1, None, [2], np.zeros((2, 3)), [0, 2, 3], [-1, 2, 3]]
+    arg = [
+        ['a', 3.2, -1, None, np.zeros((2, 3))],
+        [['a'], ['e', 2, 3]],
+        [[0, 2, 3], [-1, 2, 3]]
+        ]
     model = FullFact(3)
-    with pytest.raises(AssertionError):
-        for i in arg:
+
+    with pytest.raises(AssertionError) as e:
+        for i in arg[0]:
             model.get_exmatrix(i)
+            assert f'{i}' in f"{e.value}",\
+                f'Error: Assertion msg should contain reasons, got {e.value}'
+
+        for i in arg[1]:
+            for j in i:
+                model.get_exmatrix(j)
+                assert f'{j}' in f"{e.value}",\
+                    f'Error: Assertion msg should contain reasons, got {e.value}'
+
+        for i in arg[2]:
+            for j in i:
+                model.get_exmatrix(j)
+                assert f'{j}' in f"{e.value}",\
+                    f'Error: Assertion msg should contain reasons, got {e.value}'
 
 
 def test_getexmatrix_invalid_output(correct_input):
     model = FullFact(3)
     for i in correct_input:
-        assert isinstance(
-            model.get_exmatrix(i),
-            np.ndarray
-        ), \
-            f'Error: dtype of ematrix expected np.adarray,\
-                got {type(model.get_exmatrix(i))}'
+        ret = model.get_exmatrix(i)
+        assert isinstance(ret, np.ndarray), \
+            f'Error: dtype of ematrix expected np.adarray, got {type(ret)}'

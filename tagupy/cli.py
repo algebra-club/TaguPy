7bb031d9e2b884da74829169b8e437fe8491142e
takesei <sei06k14@gmail.com>
2021-08-30 22:32:02 +0900

    :sparkles: Add New command

--- tagupy/cli.py
+++ tagupy/cli.py
@@ -1,4 +1,5 @@
 """Console script for tagupy."""
+import os
 import sys
 from datetime import datetime
 
@@ -13,11 +14,17 @@ def main():
     return 0
 
 
+def is_project(path: str) -> bool:
+    return os.path.isfile(f'{path}/config.toml')
+
+
 @main.command(help='Create New Project')
 @click.option(
     '--name', '-n',
+    help='New Project Name',
     prompt='project name?',
-    help='New Project Name')
+    required=True,
+)
 def new(name):
     cookiecutter(
         f'{list(tagupy.__path__)[0]}/template',
@@ -27,6 +34,35 @@ def new(name):
             "current_time": datetime.now().strftime('%b-%d-%Y'),
         }
     )
+
+
+@main.command(help='Generate Experience Matrix')
+@click.option(
+    '--name', '-n',
+    help='New Experience Name',
+    prompt='experience name?',
+    required=True,
+)
+@click.option(
+    '--method', '-m',
+    prompt='generating method?',
+    help='Generate method of experience matrix',
+    type=click.Choice(tagupy.design.generator.__all__, case_sensitive=False),
+)
+def generate(name, method):
+    if not is_project(os.getcwd()):
+        click.echo('You need to locate on project root. Terminated')
+        return
+    from tagupy.manager import generator
+    method = getattr(generator, method)
+    params = method.required_params()
+    res = {}
+    for prompt, (k, t) in params.items():
+        res[k] = click.prompt(prompt, type=t)
+
+    exmatrix = method.generate(**res)
+    click.echo(name)
+    click.echo(exmatrix)
 
 
 if __name__ == "__main__":

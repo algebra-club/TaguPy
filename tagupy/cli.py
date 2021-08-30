5ec35fd48f16c249203f3ed114dbc7dc05451451
takesei <sei06k14@gmail.com>
2021-08-30 20:35:22 +0900

    :sparkles: Add cookiecutter, project generator

--- tagupy/cli.py
+++ tagupy/cli.py
@@ -1,15 +1,32 @@
 """Console script for tagupy."""
 import sys
+from datetime import datetime
+
 import click
+from cookiecutter.main import cookiecutter
+
+import tagupy
 
 
-@click.command()
-def main(args=None):
-    """Console script for tagupy."""
-    click.echo("TODO: Manage your experiment workflow"
-               "message sent from tagupy.cli.main")
-    click.echo("See Also: docs at https://click.palletsprojects.com/")
+@click.group()
+def main():
     return 0
+
+
+@main.command(help='Create New Project')
+@click.option(
+    '--name', '-n',
+    prompt='project name?',
+    help='New Project Name')
+def new(name):
+    cookiecutter(
+        f'{list(tagupy.__path__)[0]}/template',
+        no_input=True,
+        extra_context={
+            "prj_name": name,
+            "current_time": datetime.now().strftime('%b-%d-%Y'),
+        }
+    )
 
 
 if __name__ == "__main__":

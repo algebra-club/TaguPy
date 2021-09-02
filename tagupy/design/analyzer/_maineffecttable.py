d4f84bada4e2af13d55fefa6dd46cb6c0d464fe7
taka-sho <takasho@keio.jp>
2021-09-02 18:13:54 +0900

    feat(analyzer/MET) add docstring test

--- tagupy/design/analyzer/_maineffecttable.py
+++ tagupy/design/analyzer/_maineffecttable.py
@@ -22,9 +22,29 @@ class MainEffectTable(Analyzer):
 
         result: np.ndarray
 
-        Returns
+        Return
         ----------
         report: np.ndarray
+
+        Example
+        ----------
+        >>> import numpy as np
+        >>> from tagupy.design.analyzer import MainEffectTable
+        >>> analyzer = MainEffectTable()
+        >>> analyzer.analyze(
+        ...     np.array([[1, 1, 0, 1],
+        ...               [1, 1, 1, 0],
+        ...               [1, 0, 1, 1],
+        ...               [1, 0, 0, 0]]),
+        ...     np.array([[3, 4, 7],
+        ...               [4, 9, 8],
+        ...               [5, 8, 3],
+        ...               [8, 3, 6]]),
+        ... )
+        array([[ 0. ,  0. ,  0. ],
+               [-1.5,  0.5,  1.5],
+               [-0.5,  2.5, -0.5],
+               [-1. ,  0. , -1. ]])
         '''
 
         pre_result = result - result.mean(axis=0)

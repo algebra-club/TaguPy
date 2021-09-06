6cfef7fa4196d14aa0b093ca4f054b5e6f9cc28a
yo-aka-gene <yujiokano@keio.jp>
2021-09-07 00:33:44 +0900

    :recycle: delete _pb_ref from __all__ in __init__.py

--- tagupy/design/generator/__init__.py
+++ tagupy/design/generator/__init__.py
@@ -1,11 +1,9 @@
 from ._fullfact import FullFact
 from ._onehot import OneHot
 from ._plackettburman import PlackettBurman
-from . import _pb_ref
 
 __all__ = [
     "FullFact",
     "OneHot",
     "PlackettBurman",
-    "_pb_ref",
 ]

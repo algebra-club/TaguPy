e3c933e441f397d817ccbf6b324baf4456bf937b
yo-aka-gene <yujiokano@keio.jp>
2021-09-06 23:26:16 +0900

    :sparkles: add LinRegResult class

--- tagupy/design/analyzer/_lra.py
+++ tagupy/design/analyzer/_lra.py
@@ -2,10 +2,22 @@
 _Analyser Class of Linear Regression Analysis
 '''
 
-# import numpy as np
-# import statsmodels.api as sm
+import numpy as np
+import statsmodels
+import statsmodels.api as sm
 from tagupy.type import _Analyzer as Analyser
-# from tagupy.type import AnalysisResult
+from typing import Any, NamedTuple
+
+
+class LinRegResult(NamedTuple):
+    exmatrix: np.ndarray
+    resmatrix: np.ndarray
+    model: statsmodels.regression.linear_model.RegressionResultsWrapper
+    params: np.ndarray
+    bse: np.ndarray
+    predict: Any
+    rsquared: float
+    summary: Any
 
 
 class LinReg(Analyser):
@@ -18,6 +30,29 @@ class LinReg(Analyser):
 
     Notes
     -----
+    see also:
+    https://www.statsmodels.org/stable/examples/index.html
     '''
-    def __init__(self):
-        pass
+    def __init__(self, model:str):
+        """
+        Parameters
+        ----------
+        model: str
+            model for Least Squares. This arg expects either "OLS", "WLS", or "GLS".
+            see also the table below.
+
+            | model | arguments |
+            | :---: | :---:|
+            | (ordinary) Least Square | OLS |
+            | Weighted Least Square | WLS|
+            | Generalized Least Square | GLS |
+
+        """
+        assert model in ["OLS", "WLS", "GLS"], \
+            f"model expected 'OLS', 'WLS', or 'GLS', got {model}"
+        self.model = {"OLS": (0, sm.OLS), "WLS": (1, sm.OLS), "GLS": (2, sm.GLS)}[model]
+    
+    # def analyze(self, exmatrix: np.ndarray, result: np.ndarray) -> NamedTuple:
+
+
+    #     return LinRegResult()

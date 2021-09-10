40c1158e4a08c3f5865200db2cd312b31b67863c
yo-aka-gene <yujiokano@keio.jp>
2021-09-11 01:52:41 +0900

    :wrench: add statsmodels to dependencies list

---
+++ tagupy/design/analyzer/_lra.py
@@ -0,0 +1,23 @@
+'''
+_Analyser Class of Linear Regression Analysis
+'''
+
+# import numpy as np
+# import statsmodels.api as sm
+from tagupy.type import _Analyzer as Analyser
+# from tagupy.type import AnalysisResult
+
+
+class LinReg(Analyser):
+    '''
+    _Analyser Class of Linear Regression Analysis
+
+    Method
+    ------
+    analyze(exmatrix: np.ndarray, resylt:np.ndarray) -> AnalysisResult
+
+    Notes
+    -----
+    '''
+    def __init__(self):
+        pass

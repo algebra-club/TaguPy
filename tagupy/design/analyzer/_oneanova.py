e9ec56e599d641e7aa8e1692faa9d6135845cc58
gk-hazard <06ekoba13@keio.jp>
2021-09-12 14:45:47 +0900

    :construction: oneway anova

--- tagupy/design/analyzer/_oneanova.py
+++ tagupy/design/analyzer/_oneanova.py
@@ -4,10 +4,12 @@ _Analyser Class of One-way ANOVA
 
 import numpy as np
 import pandas as pd
+import statsmodels
 import statsmodels.api as sm
 
 from statsmodels.formula.api import ols
 from tagupy.type import _Analyzer as Analyzer
+from tagupy.utils import is_correct_id_list, is_int_2d_array
 from typing import List, NamedTuple
 
 
@@ -18,7 +20,7 @@ class OAResult(NamedTuple):
     exmatrix: np.ndarray
     resmatrix: np.ndarray
     model: List[List[statsmodels.regression.linear_model.RegressionResultsWrapper]]
-    table: List[np.ndarray, List[str]]
+    table: list[np.ndarray, dict[str, List[str]]]
 
 
 class OnewayANOVA(Analyzer):
@@ -35,12 +37,14 @@ class OnewayANOVA(Analyzer):
     https://www.statsmodels.org/stable/anova.html
     '''
     def __init__(self):
+        None
 
     def _input_proc(
         self,
         exmatrix: np.ndarray,
         resmatrix: np.ndarray,
-        factor_id: List[int],
+        factor: int,
+        result_id: List[int]
     ) -> pd.core.frame.DataFrame:
         '''
         process the data into the appropriate form
@@ -51,29 +55,31 @@ class OnewayANOVA(Analyzer):
             experiment matrix
         resmatrix: numpy.ndarray(number_of_runs, number_of_experiments)
             result matrix
-        factor_id: List[int]
-            list of indices of factor expected to be analyzed
-       
+        factor_id: int
+            index of factor expected to be analyzed
+        result_id: List[int]
+            list of results indices expected to be analyzed
         Return
         ------
         df_data: pd.core.frame.DataFrame
         '''
-        n_res = resmatrix.shape[1]
-        y_id = [f'y{i}' for i in range(n_res)]
-        ele, count = np.unique(exmatrix[:, factor_id], return_counts=True)
+        y_id = [f'y{i}' for i in result_id]
+        ele, count = np.unique(exmatrix[:, factor], return_counts=True)
         temp = [[e, c]for e, c in zip(ele, count)]
-        y_l = [resmatrix[exmatrix[:, factor_id] == i[0]] for i in temp]
+        result_bool = [i in result_id for i in range(resmatrix.shape[1])]
+        y_l = [resmatrix[exmatrix[:, factor] == 1][:, result_bool] for i in temp]
         data = [np.concatenate((y, np.full((i[1], 1), i[0])), axis=1) for y, i in zip(y_l, temp)]
-        df_data = pd.DataFrame(np.concatenate(temp), columns=[*y_id, 'group'])
+        df_data = pd.DataFrame(np.concatenate(data), columns=[*y_id, 'group'])
         df_data['group'] = df_data['group'].astype(str)
+
         return df_data
 
     def analyze(
         self,
         exmatrix: np.ndarray,
         resmatrix: np.ndarray,
-        factor_id: List[int] = [:],
-        result_id: List[int] = [:],
+        factor_id: List[int] = [],
+        result_id: List[int] = [],
     ) -> NamedTuple:
         '''
         return One-way ANOVA table
@@ -85,11 +91,11 @@ class OnewayANOVA(Analyzer):
         resmatrix: numpy.ndarray(number_of_runs, number_of_experiments)
             result matrix
         factor_id: List[int]
-            list of indices of factor expected to be analyzed
+            list of indices (>=0) of factors expected to be analyzed
         result_id: List[int]
-            list of indices of experimnets expected to be analyzed
+            list of indices (>=0) of experimnets expected to be analyzed
 
-        return
+        Return
         ------
         analysis_result: NamedTuple
             Result of analysis method
@@ -97,22 +103,256 @@ class OnewayANOVA(Analyzer):
             resmatrix: np.ndarray,
             model: List[List[statsmodels.regression.linear_model.RegressionResultsWrapper]]
                 list of created models of all the factor and the experiment
-            table: List[numpy.ndarray, List[str]]
-                list includes np.ndarray of One-way ANOVA table and its column name
+            table: List[np.ndarray, dict[str, List[str]]]
+                list contains One_way ANOVA table, its index name, and its column name
+
+        Note
+        ----
+        elements of factor_id, result_id expected to be non negative integer for implementation
+
+        Example
+        -------
+        >>> import numpy as np
+        >>> from tagupy.design.analyzer import OnewayANOVA
+        >>> analyzer = OnewayANOVA()
+        >>> result = analyzer.analyze(
+        ...     np.array([[-1, -1,  1, -1,  1,  1],
+        ...               [ 0,  1,  1,  1,  1,  1],
+        ...               [ 0, -1, -1, -1, -1, -1],
+        ...               [ 1, -1, -1,  0,  1,  1],
+        ...               [ 1, -1,  1, -1, -1,  0],
+        ...               [ 1,  1,  1, -1,  1, -1],
+        ...               [-1, -1, -1,  1, -1,  1],
+        ...               [ 1,  0,  1,  1, -1,  1],
+        ...               [-1,  1,  0, -1, -1,  1],
+        ...               [ 1,  1, -1,  1, -1, -1],
+        ...               [-1,  1,  1,  0, -1, -1],
+        ...               [-1,  1, -1,  1,  1,  0],
+        ...               [-1,  0, -1, -1,  1, -1],
+        ...               [-1, -1,  1,  1,  0, -1],
+        ...               [ 1,  1, -1, -1,  0,  1],
+        ...               [ 1, -1,  0,  1,  1, -1],
+        ...               [ 0,  0,  0,  0,  0,  0]]),
+        ...     np.array([[0.63019297, 0.58619756, 0.43411374],
+        ...               [0.91747724, 0.32393429, 0.04366332],
+        ...               [0.21524266, 0.81815103, 0.40212519],
+        ...               [0.88389254, 0.25744192, 0.03066148],
+        ...               [0.59410509, 0.96162365, 0.62316969],
+        ...               [0.56927398, 0.39596578, 0.76887992],
+        ...               [0.4968997 , 0.35014733, 0.74177565],
+        ...               [0.06223279, 0.17630325, 0.67672164],
+        ...               [0.63636881, 0.25071343, 0.55393937],
+        ...               [0.38908674, 0.59377007, 0.18133165],
+        ...               [0.06753605, 0.71909372, 0.67057174],
+        ...               [0.73643459, 0.25253943, 0.982259  ],
+        ...               [0.88522878, 0.37885563, 0.69005722],
+        ...               [0.63432006, 0.93461394, 0.80317958],
+        ...               [0.13611039, 0.54638813, 0.0495983 ],
+        ...               [0.57250679, 0.61633224, 0.52158236],
+        ...               [0.91144691, 0.64098009, 0.17661229]]),
+        ... )
+
+        >>> result.exmatrix
+        array([[-1, -1,  1, -1,  1,  1],
+               [ 0,  1,  1,  1,  1,  1],
+               [ 0, -1, -1, -1, -1, -1],
+               [ 1, -1, -1,  0,  1,  1],
+               [ 1, -1,  1, -1, -1,  0],
+               [ 1,  1,  1, -1,  1, -1],
+               [-1, -1, -1,  1, -1,  1],
+               [ 1,  0,  1,  1, -1,  1],
+               [-1,  1,  0, -1, -1,  1],
+               [ 1,  1, -1,  1, -1, -1],
+               [-1,  1,  1,  0, -1, -1],
+               [-1,  1, -1,  1,  1,  0],
+               [-1,  0, -1, -1,  1, -1],
+               [-1, -1,  1,  1,  0, -1],
+               [ 1,  1, -1, -1,  0,  1],
+               [ 1, -1,  0,  1,  1, -1],
+               [ 0,  0,  0,  0,  0,  0]])
+        
+        >>> result.resmatrix
+        array([[0.63019297, 0.58619756, 0.43411374],
+               [0.91747724, 0.32393429, 0.04366332],
+               [0.21524266, 0.81815103, 0.40212519],
+               [0.88389254, 0.25744192, 0.03066148],
+               [0.59410509, 0.96162365, 0.62316969],
+               [0.56927398, 0.39596578, 0.76887992],
+               [0.4968997 , 0.35014733, 0.74177565],
+               [0.06223279, 0.17630325, 0.67672164],
+               [0.63636881, 0.25071343, 0.55393937],
+               [0.38908674, 0.59377007, 0.18133165],
+               [0.73643459, 0.25253943, 0.982259  ],
+               [0.88522878, 0.37885563, 0.69005722],
+               [0.63432006, 0.93461394, 0.80317958],
+               [0.13611039, 0.54638813, 0.0495983 ],
+               [0.57250679, 0.61633224, 0.52158236],
+               [0.91144691, 0.64098009, 0.17661229]])
+        
+        >>> result.models
+        [[<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b662bfa00>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d1da90>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66fbd6d0>],
+         [<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d2cee0>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d33a30>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d3d820>],
+         [<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d42850>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66f51cd0>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66d46490>],
+         [<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66c6fd60>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66c85730>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66c857f0>],
+         [<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66991ac0>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66868d90>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b66878b50>],
+         [<statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b666dc8b0>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b666d7ac0>,
+          <statsmodels.regression.linear_model.RegressionResultsWrapper at 0x7f5b666d7a90>]]
+
+        >>> result.table[0]
+        array([[2.00000000e+00, 1.18829915e-01, 5.94149576e-02, 6.86278716e-01,
+                5.19605070e-01, 0.00000000e+00, 0.00000000e+00],
+               [1.40000000e+01, 1.21205771e+00, 8.65755505e-02,            nan,
+                           nan, 0.00000000e+00, 0.00000000e+00],
+               [2.00000000e+00, 2.17437755e-02, 1.08718877e-02, 1.61613374e-01,
+                8.52334719e-01, 1.00000000e+00, 0.00000000e+00],
+               [1.40000000e+01, 9.41793519e-01, 6.72709656e-02,            nan,
+                           nan, 1.00000000e+00, 0.00000000e+00],
+               [2.00000000e+00, 5.85845503e-01, 2.92922752e-01, 4.91419378e+00,
+                2.41676077e-02, 2.00000000e+00, 0.00000000e+00],
+               [1.40000000e+01, 8.34504845e-01, 5.96074889e-02,            nan,
+                           nan, 2.00000000e+00, 0.00000000e+00],
+               [2.00000000e+00, 4.16196925e-02, 2.08098463e-02, 2.25971531e-01,
+                8.00594416e-01, 0.00000000e+00, 1.00000000e+00],
+               [1.40000000e+01, 1.28926793e+00, 9.20905664e-02,            nan,
+                           nan, 0.00000000e+00, 1.00000000e+00],
+               [2.00000000e+00, 2.00232189e-01, 1.00116094e-01, 1.83625828e+00,
+                1.95797647e-01, 1.00000000e+00, 1.00000000e+00],
+               [1.40000000e+01, 7.63305106e-01, 5.45217933e-02,            nan,
+                           nan, 1.00000000e+00, 1.00000000e+00],
+               [2.00000000e+00, 8.67731507e-03, 4.33865754e-03, 4.30278146e-02,
+                9.58010911e-01, 2.00000000e+00, 1.00000000e+00],
+               [1.40000000e+01, 1.41167303e+00, 1.00833788e-01,            nan,
+                           nan, 2.00000000e+00, 1.00000000e+00],
+               [2.00000000e+00, 9.54397035e-02, 4.77198518e-02, 5.40757659e-01,
+                5.93993090e-01, 0.00000000e+00, 2.00000000e+00],
+               [1.40000000e+01, 1.23544792e+00, 8.82462799e-02,            nan,
+                           nan, 0.00000000e+00, 2.00000000e+00],
+               [2.00000000e+00, 5.87498162e-02, 2.93749081e-02, 4.54525204e-01,
+                6.43793174e-01, 1.00000000e+00, 2.00000000e+00],
+               [1.40000000e+01, 9.04787478e-01, 6.46276770e-02,            nan,
+                           nan, 1.00000000e+00, 2.00000000e+00],
+               [2.00000000e+00, 8.32966867e-02, 4.16483434e-02, 4.36090805e-01,
+                6.55048549e-01, 2.00000000e+00, 2.00000000e+00],
+               [1.40000000e+01, 1.33705366e+00, 9.55038330e-02,            nan,
+                           nan, 2.00000000e+00, 2.00000000e+00],
+               [2.00000000e+00, 2.01471196e-02, 1.00735598e-02, 1.07595544e-01,
+                8.98726061e-01, 0.00000000e+00, 3.00000000e+00],
+               [1.40000000e+01, 1.31074050e+00, 9.36243216e-02,            nan,
+                           nan, 0.00000000e+00, 3.00000000e+00],
+               [2.00000000e+00, 3.56920244e-02, 1.78460122e-02, 2.69273530e-01,
+                7.67801938e-01, 1.00000000e+00, 3.00000000e+00],
+               [1.40000000e+01, 9.27845270e-01, 6.62746622e-02,            nan,
+                           nan, 1.00000000e+00, 3.00000000e+00],
+               [2.00000000e+00, 1.56769145e-01, 7.83845725e-02, 8.68471304e-01,
+                4.41015367e-01, 2.00000000e+00, 3.00000000e+00],
+               [1.40000000e+01, 1.26358120e+00, 9.02558002e-02,            nan,
+                           nan, 2.00000000e+00, 3.00000000e+00],
+               [2.00000000e+00, 5.34195611e-01, 2.67097805e-01, 4.69361965e+00,
+                2.75450481e-02, 0.00000000e+00, 4.00000000e+00],
+               [1.40000000e+01, 7.96692011e-01, 5.69065722e-02,            nan,
+                           nan, 0.00000000e+00, 4.00000000e+00],
+               [2.00000000e+00, 2.10852602e-01, 1.05426301e-01, 1.96093827e+00,
+                1.77505527e-01, 1.00000000e+00, 4.00000000e+00],
+               [1.40000000e+01, 7.52684692e-01, 5.37631923e-02,            nan,
+                           nan, 1.00000000e+00, 4.00000000e+00],
+               [2.00000000e+00, 9.00871395e-02, 4.50435698e-02, 4.74049025e-01,
+                6.32112946e-01, 2.00000000e+00, 4.00000000e+00],
+               [1.40000000e+01, 1.33026321e+00, 9.50188006e-02,            nan,
+                           nan, 2.00000000e+00, 4.00000000e+00],
+               [2.00000000e+00, 1.56040354e-01, 7.80201768e-02, 9.29722956e-01,
+                4.17715104e-01, 0.00000000e+00, 5.00000000e+00],
+               [1.40000000e+01, 1.17484727e+00, 8.39176620e-02,            nan,
+                           nan, 0.00000000e+00, 5.00000000e+00],
+               [2.00000000e+00, 3.12819793e-01, 1.56409897e-01, 3.36511397e+00,
+                6.40719155e-02, 1.00000000e+00, 5.00000000e+00],
+               [1.40000000e+01, 6.50717501e-01, 4.64798215e-02,            nan,
+                           nan, 1.00000000e+00, 5.00000000e+00],
+               [2.00000000e+00, 2.00786826e-01, 1.00393413e-01, 1.15246787e+00,
+                3.44082518e-01, 2.00000000e+00, 5.00000000e+00],
+               [1.40000000e+01, 1.21956352e+00, 8.71116802e-02,            nan,
+                           nan, 2.00000000e+00, 5.00000000e+00]])
+
+        >>> result.table[1]['index']
+        ['group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual',
+         'group',
+         'Residual']
+
+        >>> result.table[1]['column']
+        ['df', 'sum_sq', 'mean_sq', 'F', 'PR(>F)', 'result_id', 'factor_id']
         '''
-        table = []
+        assert is_int_2d_array(exmatrix), \
+            f'exmatrix expected a matrix in which all elements are integer. \nGot: {exmatrix}'
+        assert isinstance(resmatrix, np.ndarray), \
+            f'resmatrix expected a numpy.ndarray, got: {type(resmatrix)}'
+        n_fac = exmatrix.shape[1]
+        n_res = resmatrix.shape[1]
+        if factor_id == []:
+            factor_id = [i for i in range(n_fac)]
+        if result_id == []:
+            result_id = [i for i in range(n_res)]
+        assert is_correct_id_list(factor_id, exmatrix), \
+            f'factor_id got incorrect input {factor_id} see the requirements described in the api.'
+        assert is_correct_id_list(result_id, exmatrix), \
+            f'result_id got incorrect input {result_id} see the requirements described in the api.'  
+        t_list = []
         model = []
-        for fac_id in range(exmatrix.shape[1]): 
-            data = _input_proc(exmatrix=exmatrix, resmatrix=resmatrix, factor_id=factor_id)
-            m = [ols(f'y{i} ~ group', data=data).fit() for i in range(n_res)]
+        for idx, fac in enumerate(factor_id):
+            data = self._input_proc(exmatrix, resmatrix, fac, result_id)
+            m = [ols(f'y{i} ~ group', data=data).fit() for i in result_id]
             t = pd.concat([sm.stats.anova_lm(i, typ=1) for i in m])
+            t['result_id'] = sum([[i, i] for i in result_id], [])
+            t['factor_id'] = [idx] * t.shape[0]
             model.append(m)
-            table.append(t)
-            result = (pd.concat(table))
+            t_list.append(t)
+        res = (pd.concat(t_list))
 
         return OAResult(
             exmatrix=exmatrix,
             resmatrix=resmatrix,
             model=model,
-            table=
+            table=[res.to_numpy(), {'index': list(res.index), 'column': list(res.columns)}]
         )
--- tagupy/utils/_validators.py
+++ tagupy/utils/_validators.py
@@ -1,6 +1,8 @@
 """
 Utility validators
 """
+import numpy as np
+
 from typing import Any, Iterable
 
 
@@ -37,6 +39,37 @@ def is_positive_int(arg: Any) -> bool:
     False
     """
     return isinstance(arg, int) and arg > 0
+
+
+def is_non_negative_int(arg: Any) -> bool:
+    """
+    Validate if the arg follows the correct form.
+
+    Parameters
+    ----------
+    arg: Any
+        arg is expected the following props.
+        1. Each element in arg, belongs to Integer, and is positive (>0).
+
+    Retrun
+    ------
+    result: Bool
+        Function returns True, if the arg following the expected props.
+        Otherwise, it returns False.
+
+    Examples
+    --------
+    >>> from tagupy.utils import is_non_negative_int
+    >>> is_non_negative_int(3)
+    True
+    >>> is_non_negative_int(0)
+    True
+    >>> is_non_negative_int(0.5)
+    False
+    >>> is_non_negative_int(-1)
+    False
+    """
+    return isinstance(arg, int) and arg >= 0
 
 
 def is_positive_int_list(arg: Any) -> bool:
@@ -77,3 +110,94 @@ def is_positive_int_list(arg: Any) -> bool:
     is_pos_int = sum(map(is_positive_int, arg)) == length
 
     return is_list and is_empty and is_pos_int
+
+
+def is_non_negative_int_list(arg: Any) -> bool:
+    """
+    Validate if the arg follows the correct form.
+
+    Parameters
+    ----------
+    arg: Any
+        arg is expected the following props.
+        1. arg belongs to Iterable[int], being able to cast to List, and not empty
+        2. Each element in arg, belongs to Integer and is not negative (0 >=).
+
+    Retrun
+    ------
+    result: Bool
+        Function returns True, if the arg following the expected props.
+        Otherwise, it returns False.
+
+    Examples
+    --------
+    >>> from tagupy.utils import is_non_negative_int_list
+    >>> is_non_negative_int_list([1, 2, 3])
+    True
+    >>> is_non_negative_int_list([1, 2, 0])
+    True
+    >>> is_non_negative_int_list([1, 0.2, -3])
+    False
+    >>> is_non_negative_int_list('string')
+    False
+    >>> is_non_negative_int_list([])
+    False
+    """
+    map(is_non_negative_int, arg)
+    is_list = isinstance(arg, Iterable)
+    length = sum(1 for _ in arg)
+    is_empty = length > 0
+    is_non_neg_int = sum(map(is_non_negative_int, arg)) == length
+
+    return is_list and is_empty and is_non_neg_int
+
+
+def is_correct_id_list(id: Any, matrix: np.ndarray) -> bool:
+    '''
+    Validate if the id follows the correct form
+
+    Parameters
+    ----------
+    id: Any
+        id is expected the following props.
+        1. id belongs to Iterable[int], being able to cast to List, and not empty
+        2. Each element in id, belongs to Integer and is not negative (0 >=).
+        3. Each element in id does not overlap
+        4. Each element in id　expected to be smaller than (column length -1)
+    matrix: np.ndarray
+        suppose exmatrix and resmatrix
+
+    Return
+    ------
+    result: Bool
+        Function returns True, if the arg following the expected props.
+        Otherwise, it returns False.
+    Examples
+    --------
+    >>> from tagupy.utils import is_correct_id_list
+    >>> matrix = np.array([[1, 1, 0, 1],
+    ...                    [1, 1, 1, 0],
+    ...                    [1, 0, 1, 1],
+    ...                    [1, 0, 0, 0]])
+    >>> is_correct_id_list([0, 1, 2, 3], matrix)
+    True
+    >>> is_correct_id_list([0, 2], matrix)
+    True
+    >>> is_correct_id_list([0, 1, 2, 3, 3], matrix)
+    False
+    >>> is_correct_id_list([4], matrix)
+    False
+    '''
+    is_nonneg_int_list = is_non_negative_int_list(id)
+    assert is_nonneg_int_list, \
+        f'id list expected list of non negative integer (0>=) got: {type(id)}:{id}'
+    length = sum(1 for _ in id)
+    is_non_dup = length == len(set(id))
+    assert is_non_dup, \
+        f'each element in id list expected not to be duplicated got: {id}'
+    max_id = max(id)
+    is_cor_id = max_id <= matrix.shape[1] - 1
+    assert is_cor_id, \
+        f'index in id list is out of range, expected <={matrix.shape[1] - 1} got:{max_id}'
+
+    return is_nonneg_int_list and is_non_dup and is_cor_id
